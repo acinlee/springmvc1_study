e4d5e6fb763204c1cdb20594136de24fd67364be
이동준 <leedongjun181027@gmail.com>
2023-01-21 16:35:35 +0900

    mvc 1편 환경 구성

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello") // name = 서블릿 이름, urlPatterns = URL 매핑 (name과 urlPatterns는 중복되면 안됨)
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+        
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
